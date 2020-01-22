83eaa99dedab2a8204c1eae45aafba4c1a6c359b
sumanthkukatla <58840292+sumanthkukatla@users.noreply.github.com>
2020-01-22 09:40:34 +0530

    Add files via upload

---
+++ main (2).c
@@ -0,0 +1,34 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int a[10][10],i,j,n,fs=0,rs=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            if(i==j)
+            fs=fs+a[i][j];
+            if(i+j==n-1)
+            rs=rs+a[i][j];
+        }
+    }
+    printf("%d",fs-rs);
+    return 0;
+}
